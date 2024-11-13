d608e51989a902e220c92556d86c69e0c2144423
Vincent <vincentbullion@icloud.com>
2024-11-13 18:06:50 +0100

    Initial Commit

---
+++ Calculato-bis/Calculato_bisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculato_bisApp.swift
+//  Calculato-bis
+//
+//  Created by Vincent Bullion on 13/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Calculato_bisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculato-bisTests/Calculato_bisTests.swift
@@ -0,0 +1,17 @@
+//
+//  Calculato_bisTests.swift
+//  Calculato-bisTests
+//
+//  Created by Vincent Bullion on 13/11/2024.
+//
+
+import Testing
+@testable import Calculato_bis
+
+struct Calculato_bisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
