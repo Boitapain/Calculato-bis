07748c1117ceea37b52bb256cf9fdbe969a94efc
Vincent <vincentbullion@icloud.com>
2024-11-15 09:09:54 +0100

    Finished app

--- Calculato-bis/ContentView.swift
+++ Calculato-bis/ContentView.swift
@@ -57,6 +57,8 @@ struct ContentView: View {
     @State private var display = "0"
     @State private var currentInput = ""
     @State private var lastOperator = ""
+    @State private var previousValue: Double = 0
+    @State private var isZeroButtonPressed: Bool = false
     
     let gridItems = [GridItem(.adaptive(minimum: .infinity, maximum: .infinity)), GridItem(.adaptive(minimum: .infinity, maximum: .infinity)), GridItem(.adaptive(minimum: .infinity, maximum: .infinity)),GridItem(.adaptive(minimum: .infinity, maximum: .infinity))]
     
@@ -76,25 +78,25 @@ struct ContentView: View {
                         .buttonStyle(CalculatorButtonStyle(isPlusMinus: true))
                         
                     Button("%") {percent()}.buttonStyle(CalculatorButtonStyle())
-                    Button("÷") {}.buttonStyle(ActionButtonStyle())
+                    Button("÷") {operation(selectedOperator: "/")}.buttonStyle(ActionButtonStyle())
                 }
                 GridRow{
                     Button("1") {appendNumber(sNum: "1")}.buttonStyle(NumberButtonStyle())
                     Button("2") {appendNumber(sNum: "2")}.buttonStyle(NumberButtonStyle())
                     Button("3") {appendNumber(sNum: "3")}.buttonStyle(NumberButtonStyle())
-                    Button("×") {}.buttonStyle(ActionButtonStyle())
+                    Button("×") {operation(selectedOperator: "*")}.buttonStyle(ActionButtonStyle())
                 }
                 GridRow{
                     Button("4") {appendNumber(sNum: "4")}.buttonStyle(NumberButtonStyle())
                     Button("5") {appendNumber(sNum: "5")}.buttonStyle(NumberButtonStyle())
                     Button("6") {appendNumber(sNum: "6")}.buttonStyle(NumberButtonStyle())
-                    Button("-") {}.buttonStyle(ActionButtonStyle())
+                    Button("-") {operation(selectedOperator: "-")}.buttonStyle(ActionButtonStyle())
                 }
                 GridRow{
                     Button("7") {appendNumber(sNum: "7")}.buttonStyle(NumberButtonStyle())
                     Button("8") {appendNumber(sNum: "8")}.buttonStyle(NumberButtonStyle())
                     Button("9") {appendNumber(sNum: "9")}.buttonStyle(NumberButtonStyle())
-                    Button("+") {}.buttonStyle(ActionButtonStyle())
+                    Button("+") {operation(selectedOperator: "+")}.buttonStyle(ActionButtonStyle())
                 }
                 GridRow{
                     Button(action: {appendNumber(sNum: "0")}) {
@@ -104,17 +106,23 @@ struct ContentView: View {
                     }
                     .padding()
                     .foregroundColor(.white)
-                    .background(Color(UIColor.gray))
+                    .background(isZeroButtonPressed ? Color(.systemGray3) : Color(UIColor.gray))
                     .font(.largeTitle)
                     .shadow(radius: 3)
                     .gridCellColumns(2)
                     .cornerRadius(.infinity)
+                    .onLongPressGesture(minimumDuration: 0, pressing: { pressing in
+                                            isZeroButtonPressed = pressing
+                                        }) {
+                                            appendNumber(sNum: "0")
+                                        }
+                    
                     Button(",") {
                       if (!currentInput.contains(".")) {
                         appendNumber(sNum: ".");
                       }
                     }.buttonStyle(NumberButtonStyle())
-                    Button("=") {}.buttonStyle(ActionButtonStyle())
+                    Button("=") {equals()}.buttonStyle(ActionButtonStyle())
                 }
             }.frame(height: 500)
         }
@@ -152,10 +160,56 @@ struct ContentView: View {
             currentInput = String(result)
             display = currentInput
         } else {
-            currentInput = "Error"
-            display = currentInput
+            currentInput = ""
+            display = "Error"
         }
     }
+    
+    func operation(selectedOperator: String) {
+        if !lastOperator.isEmpty && !currentInput.isEmpty {
+                calculate()
+        }
+        
+        lastOperator = selectedOperator
+        if let currentValue = Double(currentInput) {
+            previousValue = currentValue
+            currentInput = ""
+        }
+    }
+    
+    func calculate() {
+        if let currentValue = Double(currentInput) {
+            var result: Double = 0
+
+            switch lastOperator {
+            case "+":
+                result = previousValue + currentValue
+            case "-":
+                result = previousValue - currentValue
+            case "*":
+                result = previousValue * currentValue
+            case "/":
+                if currentValue != 0 {
+                    result = previousValue / currentValue
+                } else {
+                    return
+                }
+            default:
+                return
+            }
+            currentInput = String(result)
+            display = currentInput
+            previousValue = result
+            lastOperator = ""
+        }
+    }
+    
+    func equals() {
+        calculate()
+        lastOperator = ""
+        previousValue = Double(currentInput) ?? 0
+    }
+
 }
 #Preview {
     ContentView()
